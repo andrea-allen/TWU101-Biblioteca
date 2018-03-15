474e9c2716e6f44dbfd19972f7241d86771b4789
Andrea Allen <aallen@thoughtworks.com>
2018-03-15 16:11:20 -0500

    Andy Phoebe Welcome Message story; shouldPrintWelcomeGreeting passes.

--- src/main/java/WelcomeGreeter.java
+++ src/main/java/WelcomeGreeter.java
@@ -1,2 +1,13 @@
+import java.io.PrintStream;
+
 public class WelcomeGreeter {
+    private PrintStream printStream;
+
+    public WelcomeGreeter(PrintStream printStream){
+        this.printStream=printStream;
+    }
+
+    public void printGreeting() {
+        printStream.println("Welcome to Biblioteca!");
+    }
 }
--- src/test/java/WelcomeGreeterTest.java
+++ src/test/java/WelcomeGreeterTest.java
@@ -2,7 +2,19 @@ import org.junit.Test;
 
 import java.io.PrintStream;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.mock;
+
 
 public class WelcomeGreeterTest {
 
+    private PrintStream printStream;
+
+    @Test
+    public void shouldPrintWelcomeGreeting() {
+        printStream = mock(PrintStream.class);
+        WelcomeGreeter welcomeGreeter = new WelcomeGreeter(printStream);
+        welcomeGreeter.printGreeting();
+        verify(printStream).println("Welcome to Biblioteca!");
+    }
 }
